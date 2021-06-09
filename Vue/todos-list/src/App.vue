42a5ff26e7e258f4fef8160bc220b900407eff1b
Do Duc Duy <doducduy.vietnam@gmail.com>
2021-06-09 20:20:06 +0700

    todos-list

---
+++ Vue/todos-list/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todos
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
